402fac68f48a1f05b947302b6cf41846a99cab7b
Juan <Juan@JUAN>
2021-01-04 23:26:30 -0300

    db connection

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -1,16 +1,18 @@
-const mongoose = require('mongoose')
-require('dotenv').config();
-
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose
-    .connect(`mongodb://${process.env.DB_NAME}:${process.env.DB_PASS}@localhost:${process.env.DB_PORT}/todos`,{ useNewUrlParser: true })
-    .then(()=>{
-        console.log("DB Connected Successfully")
-    })
-    .catch(e => {
-        console.error('Connection error', e.message)
-    })
+  .connect(
+    `mongodb://${process.env.MONGO_INITDB_USERNAME}:${process.env.MONGO_INITDB_PASSWORD}@${process.env.MONGO_URL}/${process.env.MONGO_INITDB_DATABASE}`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("DB Connected Successfully");
+  })
+  .catch((e) => {
+    console.error("Connection error", e.message);
+  });
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
-module.exports = db
+module.exports = db;
